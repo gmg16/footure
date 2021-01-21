522cff5e5e0298107ac3f7aa9245956fca4a681e
gmg16 <56550793+gmg16@users.noreply.github.com>
2021-01-21 17:51:52 -0300

    Update footure.py

--- footure.py
+++ footure.py
@@ -27,3 +27,21 @@ pic = st.selectbox("Análises de Mercado", list(dic_imagens.keys()))
 st.image(dic_imagens[pic], use_column_width=True)
 
 #################################### Analíticos
+arquivos = glob.glob('analise/*.jpg')
+arquivos = sorted(arquivos)
+
+lista_nomes = []
+for arquivo in arquivos:
+  nome = (arquivo.split('_')[0])
+  lista_nomes.append(nome)
+
+jogadores = list(pd.DataFrame(lista_nomes)[0].unique())
+
+dic_imagens = {}
+for nome in jogadores:
+  nome_jogador = nome
+  slides = glob.glob(f'{nome_jogador}*.jpg')
+  slides = sorted(slides)
+  dic_imagens.update({nome:slides})
+pic = st.selectbox("Analíticos", list(dic_imagens.keys()))
+st.image(dic_imagens[pic], use_column_width=True)
