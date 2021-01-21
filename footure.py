f74cdd5dfed3015f7ea31f1becb272947a8de183
gmg16 <56550793+gmg16@users.noreply.github.com>
2021-01-21 17:57:24 -0300

    Update footure.py

--- footure.py
+++ footure.py
@@ -39,8 +39,8 @@ jogadores = list(pd.DataFrame(lista_nomes)[0].unique())
 
 dic_imagens = {}
 for nome in jogadores:
-  nome_jogador = nome
-  slides = glob.glob(f'{nome_jogador}*.jpg')
+  nome_jogador = nome.split('/')[-1]
+  slides = glob.glob(f'{analise/nome_jogador}*.jpg')
   slides = sorted(slides)
   dic_imagens.update({nome:slides})
 pic = st.selectbox("Analíticos", list(dic_imagens.keys()))
